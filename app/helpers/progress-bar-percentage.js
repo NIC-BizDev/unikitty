55cce2fecfdaf111cd48000c50af1ea6b5da8475
Lawrence Francell <lawrence@twintails.com>
2015-06-30 14:57:18 -0500

    added //TODO: for bind-attr on effect.hbs

--- app/helpers/progress-bar-percentage.js
+++ app/helpers/progress-bar-percentage.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-
+//TODO: This Should be handled with a bind-attr style or width on the elements in effet.hbs
 export function progressBarPercentage(percent) {
   return ("width: " + percent + "%").htmlSafe();
 }
